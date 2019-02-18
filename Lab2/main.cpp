dd9faf02f540c96a99bafad22e7e5f89df8d8985
niamhcoleman <35526308+niamhcoleman@users.noreply.github.com>
2019-02-18 16:14:33 +0000

    Lab 2 Files

---
+++ Lab2/main.cpp
@@ -0,0 +1,57 @@
+/*
+ *Project: CDD Labs
+ *Author: Niamh C
+ *License: GNU
+ *Description: Thread executing taskTwo must wait for thread executing taskTwo t
+ */
+
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+
+/*! \fn void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay)
+    \brief Description of void taskOne function
+    \param theSemaphore share semaphore between two threads created in main
+    \param delay used for sleep() function
+    \details This function will always run first while other waits, then signals other to begin
+*/
+
+
+void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay){
+  sleep(delay);
+  std::cout <<"I ";
+  std::cout << "must ";
+  std::cout << "print ";
+  std::cout << "first"<<std::endl;
+  theSemaphore->Signal();
+}
+/*! \fn void taskTwo(std::shared_ptr<Semaphore> theSemaphore)
+    \brief Description of void taskTwo function
+    \param theSemaphore shared semaphore between two threads created in main
+    \details This function runs second once it has been signaled by taskOne
+
+*/
+void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+  theSemaphore->Wait();
+  std::cout <<"This ";
+  std::cout << "will ";
+  std::cout << "appear ";
+  std::cout << "second"<<std::endl;
+}
+
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem( new Semaphore);
+  /**< Launch the threads  */
+  int taskOneDelay=5;
+  threadOne=std::thread(taskTwo,sem);
+  threadTwo=std::thread(taskOne,sem,taskOneDelay);
+  std::cout << "Launched from the main\n";
+   /**< Wait for the threads to finish */
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
